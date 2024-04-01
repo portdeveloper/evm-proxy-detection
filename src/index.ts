b049e3c95b8ab1ea1addb9da7e63cfa02ad3aaea
port <108868128+portdeveloper@users.noreply.github.com>
2024-04-02 00:53:31 +0300

    WIP:

--- src/index.ts
+++ src/index.ts
@@ -1,206 +1,153 @@
-import { BlockTag, EIP1193ProviderRequestFunc } from './types'
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
 
-// obtained as bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
+export const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
 const EIP_1967_LOGIC_SLOT =
   '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc'
-
-// obtained as bytes32(uint256(keccak256('eip1967.proxy.beacon')) - 1)
 const EIP_1967_BEACON_SLOT =
   '0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50'
-
-// obtained as keccak256("org.zeppelinos.proxy.implementation")
 const OPEN_ZEPPELIN_IMPLEMENTATION_SLOT =
   '0x7050c9e0f4ca769c69bd3a8ef740bc37934f8e2c036e5a723fd8ee048ed3f8c3'
-
-// obtained as keccak256("PROXIABLE")
 const EIP_1822_LOGIC_SLOT =
   '0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7'
-
 const EIP_1167_BEACON_METHODS = [
-  // bytes4(keccak256("implementation()")) padded to 32 bytes
   '0x5c60da1b00000000000000000000000000000000000000000000000000000000',
-  // bytes4(keccak256("childImplementation()")) padded to 32 bytes
-  // some implementations use this over the standard method name so that the beacon contract is not detected as an EIP-897 proxy itself
   '0xda52571600000000000000000000000000000000000000000000000000000000',
 ]
-
 const EIP_897_INTERFACE = [
-  // bytes4(keccak256("implementation()")) padded to 32 bytes
   '0x5c60da1b00000000000000000000000000000000000000000000000000000000',
 ]
-
 const GNOSIS_SAFE_PROXY_INTERFACE = [
-  // bytes4(keccak256("masterCopy()")) padded to 32 bytes
   '0xa619486e00000000000000000000000000000000000000000000000000000000',
 ]
-
 const COMPTROLLER_PROXY_INTERFACE = [
-  // bytes4(keccak256("comptrollerImplementation()")) padded to 32 bytes
   '0xbb82aa5e00000000000000000000000000000000000000000000000000000000',
 ]
 
-const detectProxyTarget = (
-  proxyAddress: string,
-  jsonRpcRequest: EIP1193ProviderRequestFunc,
-  blockTag: BlockTag = 'latest'
-): Promise<string | null> =>
-  Promise.any([
-    // EIP-1167 Minimal Proxy Contract
-    jsonRpcRequest({
-      method: 'eth_getCode',
-      params: [proxyAddress, blockTag],
-    })
-      .then(parse1167Bytecode)
-      .then(readAddress),
-
-    // EIP-1967 direct proxy
-    jsonRpcRequest({
-      method: 'eth_getStorageAt',
-      params: [proxyAddress, EIP_1967_LOGIC_SLOT, blockTag],
-    }).then(readAddress),
-
-    // EIP-1967 beacon proxy
-    jsonRpcRequest({
-      method: 'eth_getStorageAt',
-      params: [proxyAddress, EIP_1967_BEACON_SLOT, blockTag],
-    })
-      .then(readAddress)
-      .then((beaconAddress) =>
-        jsonRpcRequest({
-          method: 'eth_call',
-          params: [
-            {
-              to: beaconAddress,
-              data: EIP_1167_BEACON_METHODS[0],
-            },
-            blockTag,
-          ],
-        }).catch(() =>
-          jsonRpcRequest({
-            method: 'eth_call',
-            params: [
-              {
-                to: beaconAddress,
-                data: EIP_1167_BEACON_METHODS[1],
-              },
-              blockTag,
-            ],
-          })
-        )
-      )
-      .then(readAddress),
-
-    // OpenZeppelin proxy pattern
-    jsonRpcRequest({
-      method: 'eth_getStorageAt',
-      params: [proxyAddress, OPEN_ZEPPELIN_IMPLEMENTATION_SLOT, blockTag],
-    }).then(readAddress),
-
-    // EIP-1822 Universal Upgradeable Proxy Standard
-    jsonRpcRequest({
-      method: 'eth_getStorageAt',
-      params: [proxyAddress, EIP_1822_LOGIC_SLOT, blockTag],
-    }).then(readAddress),
-
-    // EIP-897 DelegateProxy pattern
-    jsonRpcRequest({
-      method: 'eth_call',
-      params: [
-        {
-          to: proxyAddress,
-          data: EIP_897_INTERFACE[0],
-        },
-        blockTag,
-      ],
-    }).then(readAddress),
-
-    // GnosisSafeProxy contract
-    jsonRpcRequest({
-      method: 'eth_call',
-      params: [
-        {
-          to: proxyAddress,
-          data: GNOSIS_SAFE_PROXY_INTERFACE[0],
-        },
-        blockTag,
-      ],
-    }).then(readAddress),
-
-    // Comptroller proxy
-    jsonRpcRequest({
-      method: 'eth_call',
-      params: [
-        {
-          to: proxyAddress,
-          data: COMPTROLLER_PROXY_INTERFACE[0],
-        },
-        blockTag,
-      ],
-    }).then(readAddress),
-  ]).catch(() => null)
-
-const readAddress = (value: unknown): string => {
+const readAddress = (value: any): `0x${string}` => {
   if (typeof value !== 'string' || value === '0x') {
     throw new Error(`Invalid address value: ${value}`)
   }
-
-  let address = value
-  if (address.length === 66) {
-    address = '0x' + address.slice(-40)
-  }
-
+  const address = value.length === 66 ? '0x' + value.slice(-40) : value
   const zeroAddress = '0x' + '0'.repeat(40)
   if (address === zeroAddress) {
     throw new Error('Empty address')
   }
+  return address as `0x${string}`
+}
+
+const parse1167Bytecode = (bytecode: string): string => {
+  const EIP_1167_BYTECODE_PREFIX = '0x363d3d373d3d3d363d'
+  const EIP_1167_BYTECODE_SUFFIX = '57fd5bf3'
+
+  if (!bytecode.startsWith(EIP_1167_BYTECODE_PREFIX)) {
+    throw new Error('Not an EIP-1167 bytecode')
+  }
+
+  const startIndex = EIP_1167_BYTECODE_PREFIX.length + 2 // 2 characters for the push opcode
+
+  const suffixIndex = bytecode.indexOf(EIP_1167_BYTECODE_SUFFIX, startIndex)
+  if (suffixIndex === -1) {
+    throw new Error('Not an EIP-1167 bytecode')
+  }
+
+  const addressHex = bytecode.substring(startIndex, suffixIndex)
+
+  if (addressHex.length > 40) {
+    throw new Error(
+      'Invalid EIP-1167 bytecode: Address length exceeds 20 bytes'
+    )
+  }
+
+  const address = `0x${addressHex.padStart(40, '0')}`
 
   return address
 }
 
-const EIP_1167_BYTECODE_PREFIX = '0x363d3d373d3d3d363d'
-const EIP_1167_BYTECODE_SUFFIX = '57fd5bf3'
+const detectProxyTarget = async (
+  proxyAddress: `0x${string}`
+): Promise<string | null> => {
+  const strategies = [
+    async () => {
+      const bytecode = await publicClient.getBytecode({
+        address: proxyAddress,
+      })
+      return parse1167Bytecode(bytecode as string)
+    },
+    async () => {
+      const data = await publicClient.getStorageAt({
+        address: proxyAddress,
+        slot: EIP_1967_LOGIC_SLOT,
+      })
+      return readAddress(data)
+    },
+    async () => {
+      const data = await publicClient.getStorageAt({
+        address: proxyAddress,
+        slot: EIP_1967_BEACON_SLOT,
+      })
 
-export const parse1167Bytecode = (bytecode: unknown): string => {
-  if (
-    typeof bytecode !== 'string' ||
-    !bytecode.startsWith(EIP_1167_BYTECODE_PREFIX)
-  ) {
-    throw new Error('Not an EIP-1167 bytecode')
+      const beaconAdress = readAddress(data)
+      for (const method of EIP_1167_BEACON_METHODS) {
+        try {
+          const result = await publicClient.call({
+            data: method as `0x${string}`,
+            to: beaconAdress,
+          })
+          return readAddress(result)
+        } catch (error) {}
+      }
+      throw new Error('Beacon address resolution failed')
+    },
+    async () => {
+      const data = await publicClient.getStorageAt({
+        address: proxyAddress,
+        slot: OPEN_ZEPPELIN_IMPLEMENTATION_SLOT,
+      })
+      return readAddress(data)
+    },
+    async () => {
+      const data = await publicClient.getStorageAt({
+        address: proxyAddress,
+        slot: EIP_1822_LOGIC_SLOT,
+      })
+      return readAddress(data)
+    },
+    async () => {
+      const data = await publicClient.call({
+        data: EIP_897_INTERFACE[0] as `0x${string}`,
+        to: proxyAddress,
+      })
+      return readAddress(data)
+    },
+    async () => {
+      const data = await publicClient.call({
+        data: GNOSIS_SAFE_PROXY_INTERFACE[0] as `0x${string}`,
+        to: proxyAddress,
+      })
+      return readAddress(data)
+    },
+    async () => {
+      const data = await publicClient.call({
+        data: COMPTROLLER_PROXY_INTERFACE[0] as `0x${string}`,
+        to: proxyAddress,
+      })
+      return readAddress(data)
+    },
+  ]
+
+  for (const strategy of strategies) {
+    try {
+      const result = await strategy()
+      if (result) return result
+    } catch (error) {}
   }
 
-  // detect length of address (20 bytes non-optimized, 0 < N < 20 bytes for vanity addresses)
-  const pushNHex = bytecode.substring(
-    EIP_1167_BYTECODE_PREFIX.length,
-    EIP_1167_BYTECODE_PREFIX.length + 2
-  )
-  // push1 ... push20 use opcodes 0x60 ... 0x73
-  const addressLength = parseInt(pushNHex, 16) - 0x5f
-
-  if (addressLength < 1 || addressLength > 20) {
-    throw new Error('Not an EIP-1167 bytecode')
-  }
-
-  const addressFromBytecode = bytecode.substring(
-    EIP_1167_BYTECODE_PREFIX.length + 2,
-    EIP_1167_BYTECODE_PREFIX.length + 2 + addressLength * 2 // address length is in bytes, 2 hex chars make up 1 byte
-  )
-
-  const SUFFIX_OFFSET_FROM_ADDRESS_END = 22
-  if (
-    !bytecode
-      .substring(
-        EIP_1167_BYTECODE_PREFIX.length +
-          2 +
-          addressLength * 2 +
-          SUFFIX_OFFSET_FROM_ADDRESS_END
-      )
-      .startsWith(EIP_1167_BYTECODE_SUFFIX)
-  ) {
-    throw new Error('Not an EIP-1167 bytecode')
-  }
-
-  // padStart is needed for vanity addresses
-  return `0x${addressFromBytecode.padStart(40, '0')}`
+  return null
 }
 
 export default detectProxyTarget
--- test/detectProxyTarget.spec.ts
+++ test/detectProxyTarget.spec.ts
@@ -1,138 +1,81 @@
-import { InfuraProvider } from '@ethersproject/providers'
-import { EIP1193ProviderRequestFunc } from '../src/types'
 import detectProxyTarget from '../src'
 
 describe('detectProxyTarget -> eip1193 provider', () => {
-  const infuraProvider = new InfuraProvider(1, process.env.INFURA_API_KEY)
-  const requestFunc: EIP1193ProviderRequestFunc = ({ method, params }) =>
-    infuraProvider.send(method, params)
-
-  // TODO fix to a block number to keep test stable for eternity (requires Infura archive access)
-  const BLOCK_TAG = 'latest' // 15573889
-
-  const numberOfRuns = 5
+  const numberOfRuns = 10
 
   // test this one multiple times to ensure it's stable
   // normally it gives out the correct address, but sometimes it gives out the wrong one
   for (let i = 1; i <= numberOfRuns; i++) {
     it(`detects EIP-1967 - Zora Labs: Contract Factory on run ${i}`, async () => {
       expect(
-        await detectProxyTarget(
-          '0xF74B146ce44CC162b601deC3BE331784DB111DC1',
-          requestFunc,
-          BLOCK_TAG
-        )
+        await detectProxyTarget('0xF74B146ce44CC162b601deC3BE331784DB111DC1')
       ).toBe('0x932a29dbfc1b8c3bdfc763ef53f113486a5b5e7d')
     })
   }
 
   it('detects EIP-1967 direct proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0xA7AeFeaD2F25972D80516628417ac46b3F2604Af',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0xA7AeFeaD2F25972D80516628417ac46b3F2604Af')
     ).toBe('0x4bd844f72a8edd323056130a86fc624d0dbcf5b0')
   })
 
   it('detects EIP-1967 beacon proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0xDd4e2eb37268B047f55fC5cAf22837F9EC08A881',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0xDd4e2eb37268B047f55fC5cAf22837F9EC08A881')
     ).toBe('0xe5c048792dcf2e4a56000c8b6a47f21df22752d1')
   })
 
   it('detects EIP-1967 beacon variant proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0x114f1388fAB456c4bA31B1850b244Eedcd024136',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x114f1388fAB456c4bA31B1850b244Eedcd024136')
     ).toBe('0x36b799160cdc2d9809d108224d1967cc9b7d321c')
   })
 
   it('detects OpenZeppelin proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0xed7e6720Ac8525Ac1AEee710f08789D02cD87ecB',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0xed7e6720Ac8525Ac1AEee710f08789D02cD87ecB')
     ).toBe('0xe3f3c590e044969294b1730ad8647692faf0f604')
   })
 
   it('detects EIP-897 delegate proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0x8260b9eC6d472a34AD081297794d7Cc00181360a',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x8260b9eC6d472a34AD081297794d7Cc00181360a')
     ).toBe('0xe4e4003afe3765aca8149a82fc064c0b125b9e5a')
   })
 
   it('detects EIP-897 - NOUNS DAO', async () => {
     expect(
-      await detectProxyTarget(
-        '0x6f3E6272A167e8AcCb32072d08E0957F9c79223d',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x6f3E6272A167e8AcCb32072d08E0957F9c79223d')
     ).toBe('0xe3caa436461dba00cfbe1749148c9fa7fa1f5344')
   })
 
   it('detects EIP-1167 minimal proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0x6d5d9b6ec51c15f45bfa4c460502403351d5b999',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x6d5d9b6ec51c15f45bfa4c460502403351d5b999')
     ).toBe('0x210ff9ced719e9bf2444dbc3670bac99342126fa')
   })
 
   it('detects EIP-1167 minimal proxies with vanity addresses', async () => {
     expect(
-      await detectProxyTarget(
-        '0xa81043fd06D57D140f6ad8C2913DbE87fdecDd5F',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0xa81043fd06D57D140f6ad8C2913DbE87fdecDd5F')
     ).toBe('0x0000000010fd301be3200e67978e3cc67c962f48')
   })
 
   it('detects Gnosis Safe proxies', async () => {
     expect(
-      await detectProxyTarget(
-        '0x0DA0C3e52C977Ed3cBc641fF02DD271c3ED55aFe',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x0DA0C3e52C977Ed3cBc641fF02DD271c3ED55aFe')
     ).toBe('0xd9db270c1b5e3bd161e8c8503c55ceabee709552')
   })
 
   it("detects Compound's custom proxy", async () => {
     expect(
-      await detectProxyTarget(
-        '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B')
     ).toBe('0xbafe01ff935c7305907c33bf824352ee5979b526')
   })
 
   it('resolves to null if no proxy target is detected', async () => {
     expect(
-      await detectProxyTarget(
-        '0x5864c777697Bf9881220328BF2f16908c9aFCD7e',
-        requestFunc,
-        BLOCK_TAG
-      )
+      await detectProxyTarget('0x5864c777697Bf9881220328BF2f16908c9aFCD7e')
     ).toBe(null)
   })
 })
